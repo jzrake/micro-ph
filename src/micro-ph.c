2c66b77f1f0a1f048ba141303e8d1db2921290e7
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-03-05 13:10:58 -0500

    Added equations for the pressure and internal energy, along with tests.

--- src/micro-ph.c
+++ src/micro-ph.c
@@ -11,13 +11,13 @@ static int    ZERO_SLOPE_REPEATED = 10;
 static int    MAXIMUM_ITERATION   = 1000000;
 
 
-static double  EtaValue = 1.0; // ... mu/kT (degeneracy parameter)
-static double BetaValue = 1.0; // ... kT
+static double  EtaValue = 1.0; // mu/kT     ... degeneracy parameter
+static double BetaValue = 1.0; // me c^2/kT ... unitless inverse temperature
 
 
 double step_rk4(double (*f)(double t, double y), double t, double y, double dt)
 // -----------------------------------------------------------------------------
-// Take a 4th-order Runge-Kutta step for ODE integration.
+// Take a 4th-order Runge-Kutta step for ODE integration, returns dy.
 //
 // http://en.wikipedia.org/wiki/Runge-Kutta_methods
 //
@@ -26,10 +26,10 @@ double step_rk4(double (*f)(double t, double y), double t, double y, double dt)
 //          (3) y                solution value at time 't'
 //          (4) dt               step size
 //
-// @return: y'(t)
+// @return: dt * y'(t)
 // -----------------------------------------------------------------------------
 {
-  const double k1 = dt * f(t + 0.0*dt, y + 0.0);
+  const double k1 = dt * f(t, y);
   const double k2 = dt * f(t + 0.5*dt, y + 0.5*k1);
   const double k3 = dt * f(t + 0.5*dt, y + 0.5*k2);
   const double k4 = dt * f(t + 1.0*dt, y + 1.0*k3);
@@ -39,10 +39,18 @@ double step_rk4(double (*f)(double t, double y), double t, double y, double dt)
 
 
 // -----------------------------------------------------------------------------
-// These functions generate the integrand for the dimensionless number density
-// of electrons (np) and positrons (ne) according to Sekiguchi (2010). The
-// independent variable, x := pc. The answer is the number of particles in
-// the volume pi^2 (hbar / me c)^3
+// These functions generate the integrand for the dimensionless number density,
+// pressure, and energy density of electrons and positrons according to
+// Sekiguchi (2010). The independent variable, x := pc.
+// 
+//                               *** Units ***
+//
+// Volume ... pi^2 (hbar / me c)^3
+// Energy ... me c^2
+// -----------------------------------------------------------------------------
+
+
+// Number densities
 // -----------------------------------------------------------------------------
 double integrand_ne_42(double x, double t)
 {
@@ -53,6 +61,31 @@ double integrand_np_45(double x, double t)
   return (x*x) / (exp(BetaValue * (sqrt(1 + x*x) + 1) + EtaValue) + 1);
 }
 
+// Pressure
+// -----------------------------------------------------------------------------
+double integrand_Pe_43(double x, double t)
+{
+  const double numer = pow(x,4) / sqrt(1 + x*x);
+  return numer / (exp(BetaValue * (sqrt(1 + x*x) - 1) - EtaValue) + 1);
+}
+double integrand_Pp_46(double x, double t)
+{
+  const double numer = pow(x,4) / sqrt(1 + x*x);
+  return numer / (exp(BetaValue * (sqrt(1 + x*x) + 1) + EtaValue) + 1);
+}
+
+// Internal energy density
+// -----------------------------------------------------------------------------
+double integrand_ue_44(double x, double t)
+{
+  const double numer = pow(x,2) * (sqrt(1 + x*x) - 1);
+  return numer / (exp(BetaValue * (sqrt(1 + x*x) - 1) - EtaValue) + 1);
+}
+double integrand_up_47(double x, double t)
+{
+  const double numer = pow(x,2) * (sqrt(1 + x*x) + 1);
+  return numer / (exp(BetaValue * (sqrt(1 + x*x) + 1) + EtaValue) + 1);
+}
 
 
 
@@ -108,22 +141,69 @@ double evaluate_np_45(double eta, double beta)
   return integrate_to_infinite(integrand_np_45);
 }
 
+double evaluate_Pe_43(double eta, double beta)
+{
+  EtaValue = eta;
+  BetaValue = beta;
+  return integrate_to_infinite(integrand_Pe_43);
+}
+double evaluate_Pp_46(double eta, double beta)
+{
+  EtaValue = eta;
+  BetaValue = beta;
+  return integrate_to_infinite(integrand_Pp_46);
+}
 
-void test_number_densities()
+double evaluate_ue_44(double eta, double beta)
+{
+  EtaValue = eta;
+  BetaValue = beta;
+  return integrate_to_infinite(integrand_ue_44);
+}
+double evaluate_up_47(double eta, double beta)
+{
+  EtaValue = eta;
+  BetaValue = beta;
+  return integrate_to_infinite(integrand_up_47);
+}
+
+
+
+void microph_test_npu()
 // -----------------------------------------------------------------------------
-// Compare results of numerical integration with those giben by Mathematica's
-// NIntegrate.
+// Compare results of numerical integration with those given by Mathematica's
+// NIntegrate. Integrates the equations for number density, pressure, and
+// internal energy density.
 // -----------------------------------------------------------------------------
 {
+  const char *sep = "**************************************************\n";
+
+  printf("\ntesting number densities...\n");
+  printf(sep);
   printf("ne(1.0, 1.0) = %18.15e (8.518609867257567e+00)\n", evaluate_ne_42(1.0, 1.0));
   printf("np(1.0, 1.0) = %18.15e (2.176235567609514e-01)\n", evaluate_np_45(1.0, 1.0));
   printf("ne(1.0, 0.1) = %18.15e (4.696042780715451e+03)\n", evaluate_ne_42(1.0, 0.1));
   printf("np(1.0, 0.1) = %18.15e (6.390164655753489e+02)\n", evaluate_np_45(1.0, 0.1));
+
+  printf("\ntesting pressures...\n");
+  printf(sep);
+  printf("Pe(1.0, 1.0) = %18.15e (2.970525729398766e+01)\n", evaluate_Pe_43(1.0, 1.0));
+  printf("Pp(1.0, 1.0) = %18.15e (6.562580252817735e-01)\n", evaluate_Pp_46(1.0, 1.0));
+  printf("Pe(1.0, 0.1) = %18.15e (1.571436949436383e+05)\n", evaluate_Pe_43(1.0, 0.1));
+  printf("Pp(1.0, 0.1) = %18.15e (1.9535555332427597+04)\n", evaluate_Pp_46(1.0, 0.1));
+
+  printf("\ntesting internal energy densities...\n");
+  printf(sep);
+  printf("Pe(1.0, 1.0) = %18.15e (2.390933965258743e+01)\n", evaluate_ue_44(1.0, 1.0));
+  printf("Pp(1.0, 1.0) = %18.15e (9.540921687756961e-01)\n", evaluate_up_47(1.0, 1.0));
+  printf("Pe(1.0, 0.1) = %18.15e (1.5264037046113302+05)\n", evaluate_ue_44(1.0, 0.1));
+  printf("Pp(1.0, 0.1) = %18.15e (2.0205046238189440+04)\n", evaluate_up_47(1.0, 0.1));
+
 }
 
 
 int main()
 {
-  test_number_densities();
+  microph_test_npu();
   return 0;
 }
