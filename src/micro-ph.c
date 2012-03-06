746089d23add972717fa9e1859d2681862f78f5f
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-03-06 15:04:32 -0500

    Working on the function to evaluate pressure of requested terms.

--- src/micro-ph.c
+++ src/micro-ph.c
@@ -1,14 +1,42 @@
 
 
+/*------------------------------------------------------------------------------
+ * FILE: micro-ph.c
+ *
+ *
+ * AUTHOR: Jonathan Zrake, NYU CCPP: zrake@nyu.edu
+ *
+ *
+ * DESCRIPTION: Calculates contributions to the EOS from photons, pairs, and
+ * neutrinos.
+ *
+ *
+ * REFERENCES:
+ * 
+ * (+) Sekiguchi (2010) http://arxiv.org/abs/1009.3320
+ *
+ *
+ * NOTES:
+ * 
+ * (+) Throughout the code comments, the letter h means 'h bar'
+ *
+ *------------------------------------------------------------------------------
+ */
 
 #include <stdio.h>
 #include <math.h>
 
 
-const int mphPairs     = (1 << 0);
-const int mphNeutrinos = (1 << 1);
-const int mphPhotons   = (1 << 2);
 
+const int mphElectrons   =  (1 << 0);
+const int mphPositrons   =  (1 << 1);
+const int mphNeutrinos   =  (1 << 2);
+const int mphPhotons     =  (1 << 3);
+
+
+static const double HBAR_C           = 1.973269718e+02; // MeV-fm
+static const double ELECTRON_MASS    = 5.110998928e-01; // MeV
+static const double ATOMIC_MASS_UNIT = 9.314940612e+02; // MeV
 
 static int    MAX_INTEGRAL_ITER    = 1000000;
 static double INTEGRATE_STEP_SIZE  = 1e-2;
@@ -49,11 +77,11 @@ double step_rk4(double (*f)(double t, double y), double t, double y, double dt)
 // -----------------------------------------------------------------------------
 // These functions generate the integrand for the dimensionless number density,
 // pressure, and energy density of electrons and positrons according to
-// Sekiguchi (2010). The independent variable, x := p / m c^2.
+// Sekiguchi (2010). The independent variable, x := p/mc^2.
 // 
 //                               *** Units ***
 //
-// Volume ... pi^2 (hbar / m c)^3
+// Volume ... pi^2 (h/mc)^3
 // Energy ... m c^2
 // -----------------------------------------------------------------------------
 
@@ -96,6 +124,16 @@ double integrand_up_47(double x, double t)
 }
 
 
+// -----------------------------------------------------------------------------
+// These functions generate the integrand for the dimensionless number density,
+// pressure, and energy density of electrons and positrons assuming neutrinos
+// have zero mass. The independent variable, x := pc/kT.
+// 
+//                               *** Units ***
+//
+// Volume ... pi^2 (hc/kT)^3
+// Energy ... kT
+// -----------------------------------------------------------------------------
 double integrand_neutrino_n(double x, double t)
 {
   return (x*x) / (exp(x - EtaValue) + 1);
@@ -150,43 +188,57 @@ double integrate_to_infinite(double (*f)(double t, double y))
 
 
 
-double evaluate_ne_42(double eta, double beta)
+double evaluate_ne(double eta, double beta)
 {
   EtaValue = eta;
   BetaValue = beta;
   return integrate_to_infinite(integrand_ne_42);
 }
-double evaluate_np_45(double eta, double beta)
+double evaluate_np(double eta, double beta)
 {
   EtaValue = eta;
   BetaValue = beta;
   return integrate_to_infinite(integrand_np_45);
 }
 
-double evaluate_pe_43(double eta, double beta)
+double evaluate_pe(double eta, double beta)
 {
   EtaValue = eta;
   BetaValue = beta;
   return integrate_to_infinite(integrand_pe_43);
 }
-double evaluate_pp_46(double eta, double beta)
+double evaluate_pp(double eta, double beta)
 {
   EtaValue = eta;
   BetaValue = beta;
   return integrate_to_infinite(integrand_pp_46);
 }
-
-double evaluate_ue_44(double eta, double beta)
+double evaluate_ue(double eta, double beta)
 {
   EtaValue = eta;
   BetaValue = beta;
   return integrate_to_infinite(integrand_ue_44);
 }
-double evaluate_up_47(double eta, double beta)
+double evaluate_up(double eta, double beta)
 {
   EtaValue = eta;
   BetaValue = beta;
   return integrate_to_infinite(integrand_up_47);
+}
+double evaluate_neutrino_n(double eta)
+{
+  EtaValue = eta;
+  return integrate_to_infinite(integrand_neutrino_n);
+}
+double evaluate_neutrino_p(double eta)
+{
+  EtaValue = eta;
+  return integrate_to_infinite(integrand_neutrino_p);
+}
+double evaluate_neutrino_u(double eta)
+{
+  EtaValue = eta;
+  return integrate_to_infinite(integrand_neutrino_u);
 }
 
 
@@ -195,7 +247,7 @@ double solve_for_eta_pairs(double beta, double C)
 // -----------------------------------------------------------------------------
 // Solves the implicit equation ne(e,b) - np(e,b) = C for e := eta, where C is a
 // constant, typically the total number of positive charges in the
-// characteristic volume V0 := pi^2 (hbar / m c)^3.
+// characteristic volume V0 := pi^2 (h/mc)^3.
 // -----------------------------------------------------------------------------
 {
   int niter = 0;
@@ -205,8 +257,8 @@ double solve_for_eta_pairs(double beta, double C)
 
   while (1) {
 
-    double f0 = evaluate_ne_42(eta0, beta) - evaluate_np_45(eta0, beta) - C;
-    double f1 = evaluate_ne_42(eta1, beta) - evaluate_np_45(eta1, beta) - C;
+    double f0 = evaluate_ne(eta0, beta) - evaluate_np(eta0, beta) - C;
+    double f1 = evaluate_ne(eta1, beta) - evaluate_np(eta1, beta) - C;
 
     eta2 = eta1 - f1 * (eta1 - eta0) / (f1 - f0);
     eta0 = eta1;
@@ -229,7 +281,7 @@ double solve_for_eta_neutrino(double beta, double C)
 // -----------------------------------------------------------------------------
 // Solves the implicit equation n(e,b) = C for e := eta, where C is a constant,
 // typically the total neutrino number of a particular species in the
-// characteristic volume V0 := pi^2 (hbar / m c)^3.
+// characteristic volume V0 := pi^2 (h/mc)^3.
 // -----------------------------------------------------------------------------
 {
   int niter = 0;
@@ -239,8 +291,8 @@ double solve_for_eta_neutrino(double beta, double C)
 
   while (1) {
 
-    double f0 = evaluate_ne_42(eta0, beta) - C; // equation S2010-4.9
-    double f1 = evaluate_ne_42(eta1, beta) - C;
+    double f0 = evaluate_ne(eta0, beta) - C; // equation S2010-4.9
+    double f1 = evaluate_ne(eta1, beta) - C;
 
     eta2 = eta1 - f1 * (eta1 - eta0) / (f1 - f0);
     eta0 = eta1;
@@ -273,28 +325,28 @@ void microph_test_npu()
   printf(sep);
   const double T0[] = { 8.518609867257567, 0.21762355676095138, // true
 			4696.042780715451, 639.0164655753489 };
-  printf("ne(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_ne_42(1.0, 1.0), T0[0]);
-  printf("np(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_np_45(1.0, 1.0), T0[1]);
-  printf("ne(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_ne_42(1.0, 0.1), T0[2]);
-  printf("np(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_np_45(1.0, 0.1), T0[3]);
+  printf("ne(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_ne(1.0, 1.0), T0[0]);
+  printf("np(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_np(1.0, 1.0), T0[1]);
+  printf("ne(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_ne(1.0, 0.1), T0[2]);
+  printf("np(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_np(1.0, 0.1), T0[3]);
 
   printf("\ntesting pressures...\n");
   printf(sep);
   const double T1[] = { 29.705257293987664, 0.6562580252817735,
 			157143.69494363826, 19535.555332427597 };
-  printf("pe(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_pe_43(1.0, 1.0), T1[0]);
-  printf("pp(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_pp_46(1.0, 1.0), T1[1]);
-  printf("pe(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_pe_43(1.0, 0.1), T1[2]);
-  printf("pp(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_pp_46(1.0, 0.1), T1[3]);
+  printf("pe(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_pe(1.0, 1.0), T1[0]);
+  printf("pp(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_pp(1.0, 1.0), T1[1]);
+  printf("pe(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_pe(1.0, 0.1), T1[2]);
+  printf("pp(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_pp(1.0, 0.1), T1[3]);
 
   printf("\ntesting internal energy densities...\n");
   printf(sep);
   const double T2[] = { 23.909339652587427, 0.9540921687756961,
 			152640.37046113302, 20205.04623818944 };
-  printf("pe(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_ue_44(1.0, 1.0), T2[0]);
-  printf("pp(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_up_47(1.0, 1.0), T2[1]);
-  printf("pe(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_ue_44(1.0, 0.1), T2[2]);
-  printf("pp(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_up_47(1.0, 0.1), T2[3]);
+  printf("pe(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_ue(1.0, 1.0), T2[0]);
+  printf("pp(1.0, 1.0) = %18.15e (%18.15e)\n", evaluate_up(1.0, 1.0), T2[1]);
+  printf("pe(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_ue(1.0, 0.1), T2[2]);
+  printf("pp(1.0, 0.1) = %18.15e (%18.15e)\n", evaluate_up(1.0, 0.1), T2[3]);
 }
 
 
@@ -320,16 +372,60 @@ void microph_test_eta()
 
 
 double microph_get(int flags)
+// -----------------------------------------------------------------------------
+// Evaluate the pressure (in MeV/fm^3), including terms specified by 'flags'.
+//
+// Implemented terms are:
+//
+// (+) mphElectrons
+// (+) mphPositrons
+// (+) mphNeutrinos
+// (+) mphPhotons
+//
+// -----------------------------------------------------------------------------
 {
+  const double kT = 1.0; // MeV
+  const double eta = 1.0;
 
-  if (flags & mphPairs) {
-    printf("requested pairs\n");
+  double p = 0.0;
+
+  // Electrons
+  // ---------------------------------------------------------------------------
+  if (flags & mphElectrons) {
+    const double Volume = pow(M_PI, 2) * pow(HBAR_C/ELECTRON_MASS, 3);
+    const double Energy = ELECTRON_MASS;
+    const double beta = ELECTRON_MASS / kT;
+
+    p += (Energy / Volume) * evaluate_pe(eta, beta);
   }
+
+  // Positrons
+  // ---------------------------------------------------------------------------
+  if (flags & mphPositrons) {
+
+    const double Volume = pow(M_PI, 2) * pow(HBAR_C/ELECTRON_MASS, 3);
+    const double Energy = ELECTRON_MASS;
+    const double beta = ELECTRON_MASS / kT;
+
+    p += (Energy / Volume) * evaluate_pp(eta, beta);
+  }
+
+  // Neutrinos
+  // ---------------------------------------------------------------------------
+  if (flags & mphNeutrinos) {
+    const double Volume = pow(M_PI, 2) * pow(HBAR_C/kT, 3);
+    const double Energy = kT;
+
+    p += (Energy / Volume) * evaluate_neutrino_p(eta);
+  }
+
+  // Photons
+  // ---------------------------------------------------------------------------
   if (flags & mphPhotons) {
-    printf("requested photons\n");
+
   }
 
-  return 0.0;
+  return p;
 }
 
 
@@ -338,7 +434,7 @@ int main()
   microph_test_npu();
   microph_test_eta();
 
-  microph_get(mphPairs);
+  microph_get(mphPositrons | mphPhotons | mphNeutrinos);
 
   return 0;
 }
