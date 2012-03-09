35685cade121756e386fc9e8cccee156344b7f2b
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-03-08 20:20:37 -0500

    Still struggling with root-finder when T < 1 MeV.

--- eospy/__init__.py
+++ eospy/__init__.py
@@ -5,6 +5,14 @@ from scipy.integrate import quadpack
 from scipy.optimize import newton
 
 
+LIGHT_SPEED      = 2.997924580e+10; # cm/s
+HBAR_C           = 1.973269718e+02; # MeV-fm
+ELECTRON_MASS    = 5.110998928e-01; # MeV
+ATOMIC_MASS_UNIT = 9.314940612e+02; # MeV
+MEV_TO_ERG       = 1.602176487e-06;
+FM3_TO_CM3       = 1.000000000e-39;
+
+
 # don't worry about overflows in exp
 np.seterr(over='ignore')
 
@@ -20,7 +28,7 @@ def pdf_fermion(x, sgn, eta, beta):
     eta   : mu/kT     ... degeneracy parameter
     beta  : mc^2/kT   ... unitless inverse temperature
     
-    Notes
+    Notes:
     --------------------------------------------------------
 
     Volume             ... pi^2 (h/mc)^3
@@ -28,9 +36,9 @@ def pdf_fermion(x, sgn, eta, beta):
     """
     return x*x / (np.exp(beta*(np.sqrt(1 + x*x) - sgn*1.0) - sgn*eta) + 1.0)
 
-def ferm_number_density (*args): return 1.0
-def ferm_pressure       (*args): return np.power(x,2) / (np.sqrt(1 + x*x))
-def ferm_internal_energy(*args): return np.sqrt(1 + x*x) - args[1]*1
+def ferm_number_density (x,s,e,b): return 1.0
+def ferm_pressure       (x,s,e,b): return np.power(x,2) / (np.sqrt(1 + x*x))
+def ferm_internal_energy(x,s,e,b): return np.sqrt(1 + x*x) - s*1
 
 
 
@@ -38,14 +46,14 @@ def pdf_fermion_massless(x, sgn, eta, beta):
     """
     Returns the integrand for fermion number density, in the massless limit.
 
-    Parameters
+    Parameters:
     --------------------------------------------------------
 
     x     : pc/kT     ... independent variable
     eta   : mu/kT     ... degeneracy parameter
     beta  :           ... dummy argument
 
-    Notes
+    Notes:
     --------------------------------------------------------
 
     Volume             ... pi^2 (hc/kT)^3
@@ -53,9 +61,9 @@ def pdf_fermion_massless(x, sgn, eta, beta):
     """
     return 0.5 * sgn * (x*x) / (1 + np.cosh(x - sgn*eta))
 
-def fmml_number_density (*args): return 1.0
-def fmml_pressure       (*args): return x
-def fmml_internal_energy(*args): return x
+def fmml_number_density (x,s,e,b): return 1.0
+def fmml_pressure       (x,s,e,b): return x
+def fmml_internal_energy(x,s,e,b): return x
 
 
 def evaluate_term(key, sgn, eta, beta, massless=False):
@@ -63,7 +71,7 @@ def evaluate_term(key, sgn, eta, beta, massless=False):
     Evaluates the EOS variable 'key', which is one of 'number_density',
     'pressure', or 'internal energy'.
 
-    Notes
+    Notes:
     --------------------------------------------------------
 
     Use sgn = +1/-1 for particles and anti-particles respecticely. Eta is always
@@ -76,7 +84,7 @@ def evaluate_term(key, sgn, eta, beta, massless=False):
         "number_density": ferm_number_density,
         "pressure": ferm_pressure,
         "internal_energy": ferm_internal_energy }
-    
+
     massless_terms = {
         "pdf": pdf_fermion_massless,
         "number_density": fmml_number_density,
@@ -86,8 +94,8 @@ def evaluate_term(key, sgn, eta, beta, massless=False):
     terms = massless_terms if massless else massive_terms
     pdf, val = terms["pdf"], terms[key]
     f = lambda x: val(x, sgn, eta, beta) * pdf(x, sgn, eta, beta)
-    return quadpack.quad(f, 0.0, quadpack.Inf)[0]
-
+    res = quadpack.quad(f, 0.0, quadpack.Inf)
+    return res[0]
 
 
 def solve_eta_pairs(beta, C):
@@ -102,8 +110,20 @@ def solve_eta_pairs(beta, C):
     beta : mc^2 / kT    ... dimensionless inverse temperature
     C    :              ... dimensionless number density
     """
-    def f(eta):
-        return evaluate_n(+1, eta, beta) - evaluate_n(-1, eta, beta) - C
+    def f1(eta):
+        """
+        ne(e,b) - np(e,b) = C
+        """
+        return \
+            evaluate_term("number_density", +1, eta, beta) - \
+            evaluate_term("number_density", -1, eta, beta) - C
+    def f2(eta):
+        """
+        ne(e,b) = C, assume np -> 0.0 for large beta
+        """
+        return \
+            evaluate_term("number_density", +1, eta, beta) - C
+    f = f1 if beta < 10.0 else f2
     return newton(f, 0.0)
 
 
@@ -120,3 +140,92 @@ def solve_eta_neutrinos(C):
     def f(eta):
         return evaluate_n(+1, eta, beta) - C
     return newton(f, 0.0)
+
+
+
+def eval_pairs(D, kT, Ye, sgn):
+    """
+    Returns the number density, pressure, and internal (n,p,u) energy for
+    either electrons or positrons depending on sgn (+/-).
+
+    Parameters:
+    --------------------------------------------------------
+
+    D   : density (g/cm^3)
+    kT  : temperature (MeV)
+    Ye  : proton/electron fraction
+    sgn : (+) for electrons, (-) for positrons
+
+    Returns:
+    --------------------------------------------------------
+
+    n   : number density (1/fm^3)
+    p   : pressure (MeV/fm^3)
+    u   : internal energy (MeV/fm^3)
+    """
+    c2 = LIGHT_SPEED*LIGHT_SPEED
+    Volume = np.power(np.pi, 2) * np.power(HBAR_C/ELECTRON_MASS, 3)
+    Energy = ELECTRON_MASS
+
+    Erest = D * c2 * FM3_TO_CM3 / MEV_TO_ERG
+    C = Volume * Ye * Erest / ATOMIC_MASS_UNIT
+
+    beta = ELECTRON_MASS / kT
+    eta = solve_eta_pairs(beta, C)
+
+    nk = "number_density"
+    pk = "pressure"
+    uk = "internal_energy"
+
+    n = (1.0    / Volume) * evaluate_term(nk, sgn, eta, beta)
+    p = (Energy / Volume) * evaluate_term(pk, sgn, eta, beta)
+    u = (Energy / Volume) * evaluate_term(uk, sgn, eta, beta)
+
+    return n,p,u
+
+
+
+def eos_eval(D, kT, Ye, component):
+    """
+    Returns the number density, pressure, and internal energy given the density,
+    temperature, and internal energy.
+
+    Parameters:
+    --------------------------------------------------------
+
+    component : A list of terms to be included. May contain:
+
+    ["electrons", "positrons", "photons", "neutrinos"]
+
+    D   : density (g/cm^3)
+    kT  : temperature (MeV)
+    Ye  : proton/electron fraction
+    """
+
+    n = 0.0
+    p = 0.0
+    u = 0.0
+
+    if "electrons" in component:
+        ev = eval_pairs(D, kT, Ye, +1)
+        n += ev[0]; p += ev[1]; u += ev[2]
+
+    if "positrons" in component:
+        ev = eval_pairs(D, kT, Ye, -1)
+        n += ev[0]; p += ev[1]; u += ev[2]
+
+    if "photons" in component:
+        a = pow(np.pi, 2) / (15*np.power(HBAR_C, 3))
+        n += 0.0
+        p += a * np.power(kT, 4) / 3.0
+        u += a * np.power(kT, 4)
+
+    return n, p, u
+
+
+def convert_beta_to_T(beta):
+    """
+    Convenience function, returns the temperature (in MeV) given beta :=
+    mc^2/kT.
+    """
+    return ELECTRON_MASS / beta
--- micro-ph.py
+++ micro-ph.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python
 
-from eospy import evaluate_term
+from eospy import *
+
+
+def test_large_beta():
+    from matplotlib import pyplot as plt
+    import numpy as np
+
+    eta = np.linspace(-10.0, 10.0, 5)
+    beta = np.logspace(-2.0, 1.5, 40)
+    T = [convert_beta_to_T(b) for b in beta]
+
+    for e in eta:
+        n = [evaluate_term("number_density", -1, e, B) for B in beta]
+        plt.loglog(beta, n, label=r"$\eta = %2.1f$" % e)
+
+    plt.xlabel(r"$\beta = m_e c^2 / k_B T$", fontsize=16)
+    plt.ylabel(r"$n_+(\eta,\beta)$", fontsize=16)
+    plt.legend(loc='best')
+    plt.show()
+
+
 
 if __name__ == "__main__":
-    print evaluate_term("number_density", +1, 1, 1)
-    print evaluate_term("number_density", -1, 1, 1, massless=False)
+    #print evaluate_term("number_density", +1, 1, 1)
+    #print evaluate_term("number_density", -1, 100.0, 1000.0, massless=False)
+
+    #print eos_eval(1e13, 0.1, 0.08, ["photons"])
+    print eos_eval(1e13, 0.5, 0.08, ["electrons"])
+    #print eos_eval(1e13, 0.1, 0.08, ["positrons"])
